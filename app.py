667afe317b47f00e0347361e419788be86a9e1da
Sourav_Raj <srvraj9135@gmail.com>
2025-09-26 00:35:16 +0530

    finally resolved all errors & ready for deployment

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ import nltk
 import string 
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
+from sklearn.feature_extraction.text import CountVectorizer,TfidfTransformer
+from sklearn.naive_bayes import GaussianNB,MultinomialNB,BernoulliNB
 
 ps=PorterStemmer()
-tfidf=pickle.load(open('vectorizer.pkl','rb'))
-model=pickle.load(open('model_used.pkl','rb'))
+tfidf=pickle.load(open('vectorizer1.pkl','rb'))
+model=pickle.load(open('model_used.pkl','rb')) # multinomial naive bias 
 
 def transform_text(text):
     text=text.lower()
@@ -36,11 +38,14 @@ input_sms=st.text_input("Enter the message")
 if st.button("Predict"):
     # 1. preprocess
     transformed_sms=transform_text(input_sms)
-    # 2. vectorize
-    vector_input=tfidf.transform([transformed_sms])
-    # 3. predict
-    result=model.predict(vector_input)[0]
-    # 4. display
+    if transformed_sms and transformed_sms.strip() != "":
+        vector_input = tfidf.transform([transformed_sms])
+        result = model.predict(vector_input)[0]
+        print("Prediction:", result)
+
+    
+    else:
+        result = 0
     if result== 1:
         st.header("Spam")
     else:
